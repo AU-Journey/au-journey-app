4ff56f1f0a0300a446e7d04790eb97080f94ffde
PaingThuTa <paingthuta472@gmail.com>
2025-05-21 15:50:10 +0700

    example connection for swift and three.js

--- AU Journey/MapViewController.swift
+++ AU Journey/MapViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import WebKit
 
 class MapViewController: UIViewController {
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
+        webView = WKWebView(frame: view.bounds)
+        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(webView)
 
+        if let htmlPath = Bundle.main.path(forResource: "index", ofType: "html") {
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
 }
