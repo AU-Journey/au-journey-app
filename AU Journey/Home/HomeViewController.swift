bfa5b81782443a18b89f7b1f6b8db9343937f82b
Sorracha <109853255+beausorracha@users.noreply.github.com>
2025-05-22 19:44:28 +0700

    Update HomeViewController.swift

--- AU Journey/Home/HomeViewController.swift
+++ AU Journey/Home/HomeViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    let userGreetingsLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        style()
+        layout()
+        
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension HomeViewController {
+    private func style() {
+        
+        // userGreetingsLabel
+        userGreetingsLabel.translatesAutoresizingMaskIntoConstraints = false
+        userGreetingsLabel.font = UIFont(name: "Outfit-Medium", size: 24)
+        userGreetingsLabel.textColor = .black
+        userGreetingsLabel.textAlignment = .center
+        userGreetingsLabel.text = "Hello, Beau!"
+        
     }
     
-
+    private func layout() {
+        view.addSubview(userGreetingsLabel)
+        
+        // userGreetingsLabel
+        NSLayoutConstraint.activate([
+            userGreetingsLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            userGreetingsLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: userGreetingsLabel.trailingAnchor, multiplier: 2)
+        ])
+                                                         
+    }
 }
