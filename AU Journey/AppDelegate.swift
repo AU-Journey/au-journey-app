d43ea9765e93347f429571f2c80b51605ba3ae29
Sorracha <109853255+beausorracha@users.noreply.github.com>
2025-05-23 15:11:41 +0700

    Update AppDelegate.swift

--- AU Journey/AppDelegate.swift
+++ AU Journey/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = TabBarController()
+        window?.rootViewController = LoginViewController()
         
         return true
         
